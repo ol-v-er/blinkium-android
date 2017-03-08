f352eb8e6106906b46633ef7f18f4b57a8725d3f
ogoutet <o.goutet@openium.fr>
2017-03-08 17:04:52 +0100

    Move of the precedent activity to the correct path

--- app/src/main/java/fr/openium/blinkiumandroid/MainActivity.java
+++ app/src/main/java/fr/openium/blinkiumandroid/MainActivity.java
@@ -1,13 +1,48 @@
-package fr.openium.blinkiumandroid;
+package com.example.kevin.myapplication;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    String identifiant;
+    String password;
+
+    //Unused for the moment. Names will be changed
+    final String EXTRA_LOGIN = "user_login";
+    final String EXTRA_PASSWORD = "user_password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = (Button)findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(view.getId() == R.id.loginButton){
+                    EditText temp = (EditText)findViewById(R.id.idField);
+                    identifiant = temp.getText().toString();
+
+                    temp = (EditText)findViewById(R.id.passField);
+                    password = temp.getText().toString();
+
+                    Intent intent = new Intent(MainActivity.this, Logged_Activity.class);
+                    intent.putExtra(EXTRA_LOGIN, identifiant);
+                    intent.putExtra(EXTRA_PASSWORD, password);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
     }
 }
