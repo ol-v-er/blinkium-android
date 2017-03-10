008c62735d6c6820f2ea3e7ba1e9ee03eb72d67e
kepoton <kevin.poton@orange.fr>
2017-03-10 15:18:40 +0100

    Add of the ConvertUtils class that provides an "encode" method to convert a String into an array of bits according to this project's standard for transmission

---
+++ app/src/main/java/fr/openium/blinkiumandroid/utils/ConvertUtils.java
@@ -0,0 +1,69 @@
+package fr.openium.blinkiumandroid.utils;
+
+/**
+ * Created by Kevin on 10/03/2017.
+ */
+
+public class ConvertUtils {
+
+    public static byte[] encode(String s){
+        byte[] startCode = { 1, 0, 1};
+        byte[] header = generateHeader(s);
+        byte[] body = stringToBin(s);
+        byte[] result = new byte[startCode.length + header.length + body.length];
+
+
+        System.arraycopy(startCode, 0, result, 0, startCode.length);
+        System.arraycopy(header, 0, result, startCode.length, header.length);
+        System.arraycopy(body, 0, result, startCode.length+header.length, body.length);
+
+        return result;
+    }
+
+    private static byte[] generateHeader(String s){
+        int dataLen = (short) s.length();
+        int dataSize = 1;
+        byte[] header = new byte[8+4];
+
+        byte[] len = decToBin(dataLen, 8);
+        byte[] size = decToBin(dataSize, 4);
+
+        System.arraycopy(len, 0, header, 0, 8);
+        System.arraycopy(size, 0, header, 8, 4);
+
+        return header;
+    }
+
+    private static byte[] asciiToBin(char c){
+        return decToBin((int)c, 8);
+    }
+
+
+    private static byte[] decToBin(int dec, int size){
+        byte[] result = new byte[size];
+        int temp = dec;
+
+        //Algorithme de conversion Decimal -> Binaire
+        //Divisions succéssives par 2 et remplissage du tableau result avec le reste de ces divisions
+        for(int i = size-1; i >=0; i--){
+            result[i] = (byte) (temp % 2);
+            temp = temp / 2;
+        }
+
+        return result;
+    }
+
+    private static byte[] stringToBin(String s){
+        byte[] charBin;
+        byte[] result = new byte[s.length()*8];
+        //Conversion de la chaine
+        for(int i = 0; i < s.length(); i++){
+            //Tableau qui contiendra temporairement les 8 bits du caractère
+            charBin = asciiToBin(s.charAt(i));
+
+            System.arraycopy(charBin, 0, result, i*8, 8);
+
+        }
+        return result;
+    }
+}
